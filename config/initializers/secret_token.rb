871a7f1b67fe0b8d612a9f128cd0a7fc9a0e8e40
Jeff Leung <mr.jeffleung@gmail.com>
2014-08-18 10:25:59 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BukkuApp::Application.config.secret_key_base = '34feba344531ce100c5a5f4158056761b9226f6ada0ba4bb80d949c0c0840a71f986751c77308cb20dea0a27a9059568eb421c96c71d7b6321a9ca22123e2a10'
